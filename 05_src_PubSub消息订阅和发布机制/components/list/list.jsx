457a483fc9d0fee38926a34a891f7499efb7a916
xia xiaoxia <1225003025@qq.com>
2020-02-08 00:07:13 +0800

    submit Route case

--- 05_src_PubSub消息订阅和发布机制/components/list/list.jsx
+++ 05_src_PubSub消息订阅和发布机制/components/list/list.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import Item from '../item/item';
+import PubSub from 'pubsub-js'
 
 export default class List extends Component {
+    //使用消息订阅和发布机制的方式进行组件通信
+	state = {
+		isFirst:true,//标识是否是第一次打开
+		isLoading:false,//标识是否加载中
+		error:'',//错误信息
+		users:[]//所有用户信息
+	}
+    
+    //使用生命周期方法进行挂载
+    componentDidMount(){
+        //在挂载里边可以1、开启定时器、2、发送网络请求、3、订阅消息
+        PubSub.subscribe('atguigu',(msg,data)=>{
+            //箭头函数中的msg不能删除
+            this.setState(data)
+        })
+    }
+
     //重写父类继承render
     render() {
-        //接收App传过来的users
-        const { users, error, isLoading, isFirst } = this.props;
+        
+        const { users, error, isLoading, isFirst } = this.state;
         if (isFirst) {
             return <h2>输入关键词，点击搜索</h2>
         } else if (isLoading) {
--- 05_src_PubSub消息订阅和发布机制/components/search/search.jsx
+++ 05_src_PubSub消息订阅和发布机制/components/search/search.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PubSub from 'pubsub-js'
 export default class Search extends Component {
     //写button的点击事件
     search = () => {
@@ -11,25 +12,22 @@ export default class Search extends Component {
             alert('内容不能为空')
         }
         //2、发送请求之前，展示loading状态
-        this.props.updateAppState({
-            isFirst:false,
-            isLoading:true
-        })
+        PubSub.publish('atguigu',{isFirst:false,isLoading:true});
         
         //4、发送请求 根据返回的数据更新状态
         /* 但更新状态，在这个组件里是无法直接更新App的组件的状态 的，因此需要在App.js中进行更新状态 */
-        axios.get('https://api.githubb.com/search/users', { params: { q: value } }).then(
+        axios.get('https://api.github.com/search/users', { params: { q: value } }).then(
             (response) => {
                 //更新成功的状态
-                this.props.updateAppState({
-                    isLoading: false,
-                    users: response.data.items
+                PubSub.publish('atguigu',{
+                    isLoading:false,
+                    users:response.data.items
                 })
                 // console.log(response.data.items)
             },
             (error) => {
                 //更新失败的状态
-                this.props.updateAppState({
+                PubSub.publish('atguigu',{
                     isLoading: false,
                     error:error.message
                 })
